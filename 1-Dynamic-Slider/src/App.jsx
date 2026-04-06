3f27de85486912ccee795b7aaad58098bc2ae207
aslamlaiba713 <aslamlaiba713@gmail.com>
2026-04-06 19:11:31 +0500

    Initial commit

---
+++ 1-Dynamic-Slider/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSlider from './components/HeroSlider'
+
+const App = () => {
+  return (
+    <div>
+      <HeroSlider/>
+    </div>
+  )
+}
+
+export default App
---
+++ 1-Dynamic-Slider/src/components/HeroSlider.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import data from "../Data/ProjectData.json";
+
+const HeroSlider = () => {
+  const [index, setIndex] = useState(0);
+
+   const nextSlide = () => {
+    setIndex((prev) => (prev + 1) % data.length);
+  };
+
+  const prevSlide = () => {
+    setIndex((prev) => (prev - 1 + data.length) % data.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
+  return (
+    <section className="slider">
+      <div
+        className="slides"
+        style={{ transform: `translateX(-${index * 100}%)`, display: "flex" }}
+      >
+        {data.map((item, i) => (
+          <div className="slide" key={i} style={{ minWidth: "100%" }}>
+            <img src={item.image} alt={item.title} />
+            <div className="content">
+              <h1>{item.title}</h1>
+              <p>{item.desc}</p>
+              <button>{item.btn}</button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {/* Buttons */}
+      <button className="arrow left" onClick={prevSlide}>
+        ❮
+      </button>
+      <button className="arrow right" onClick={nextSlide}>
+        ❯
+      </button>
+
+      {/* Dots */}
+      <div className="dots">
+        {data.map((_, i) => (
+          <span
+            key={i}
+            className={i === index ? "dot active" : "dot"}
+            onClick={() => setIndex(i)}
+          ></span>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default HeroSlider;
