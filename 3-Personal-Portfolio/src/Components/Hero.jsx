3ca28f09f1f79138150fb7ac3c14bb83e76df5b5
aslamlaiba713 <aslamlaiba713@gmail.com>
2026-04-08 22:56:10 +0500

    update hero

--- 3-Personal-Portfolio/src/Components/Hero.jsx
+++ 3-Personal-Portfolio/src/Components/Hero.jsx
@@ -13,9 +13,9 @@ const Hero = () => {
           className="w-full h-full object-cover opacity-80"
         />
 
-        <div className="absolute inset-0 flex flex-col justify-center text-white px-4 ml-20">
-          <p className='text-cyan-300 bg-cyan-300 border-cyan-300 bg-transparent border-2 w-40 rounded-2xl p-1'>🔵 Open To Work</p>
-          <h1 className="text-6xl md:text-6xl font-bold mb-4 text-4xl md:text-6xl lg:text-8xl">
+        <div className="absolute inset-0 flex flex-col justify-center text-white px-6 md:px-16">
+          <p className='text-cyan-300 border-cyan-300 border-2 w-40 rounded-2xl p-1'>🔵 Open To Work</p>
+          <h1 className=" font-bold mb-4 text-4xl md:text-6xl lg:text-8xl">
             <span>We Craft</span><br /> 
             <span className='text-cyan-300'>Digital</span> <br /> 
             <span>Experiences</span>
